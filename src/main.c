afd311471d3001d1aae89c1bd15febe783dba137
David Nicholson <nickledave@users.noreply.github.com>
2026-08-15 08:47:51 -0400

    Rewrite main to print each line using getline

--- src/main.c
+++ src/main.c
@@ -7,18 +7,17 @@ int main(int argc, char *argv[]) {
     }
 
     FILE *fp;
+    char *line = NULL;
+    size_t len = 0;
+    size_t read;
+    
     fp = fopen(argv[1], "r");
     if (fp == NULL) {
         puts("Cannot open file");
     }
 
-    char ch;
-    while (1)
+    while ((read = getline(&line, &len, fp)) != -1)
     {
-        ch = fgetc(fp);
-        if (ch == EOF)
-            break;
-        else 
-            printf("%c", ch);
+        printf("%s", line);
     }
 }
