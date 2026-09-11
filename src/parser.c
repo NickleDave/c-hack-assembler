79fe8526ba5cc0ebc32624e618193e28dbe70aec
David Nicholson <nickledave@users.noreply.github.com>
2026-09-10 23:55:48 -0400

    Add logic to parse labels and C-commands to parser

--- src/parser.c
+++ src/parser.c
@@ -105,6 +105,16 @@ bool is_valid_symbol(char str[]) {
 }
 
 
+int char_count(char * str, char char_) {
+    int count = 0;
+    int ind = -1;
+    while (str[++ind]) {
+        if (str[ind] == char_) { count += 1;}
+    }
+    return count;
+}
+
+
 struct Command parse_line(char line[]) {
     if (strcmp(line, "\n\0") == 0) {
         struct Command command = {EMPTY_LINE};
@@ -162,6 +172,77 @@ struct Command parse_line(char line[]) {
                 exit(1);
             }
         }
+    } else if (line_no_whitespace[0] == '(') {
+        int line_len = strlen(line_no_whitespace);
+
+        // error if we don't find terminating ')'
+        if (!line_no_whitespace[line_len - 1] == ')') {
+                fprintf(
+                    stderr,
+                    "parser::parse_line: line begins with '(' but did not end with ')': %s",
+                    line_no_whitespace 
+                );
+                exit(1);
+        }
+
+        // now get the "l-value" between the two parentheses
+        int len = (line_len - 1) - 1;
+        char *l_value = (char*)malloc((len + 1) * sizeof(char));
+        strncpy(l_value, line_no_whitespace + 1, len);
+        l_value[len] = '\0';
+        // and if it's a valid symbol, return the command
+        if (is_valid_symbol(l_value)) {
+            struct Command command = {L_COMMAND, .symbol=l_value};
+            return command;
+        } else {
+            fprintf(
+                stderr,
+                "parser::parse_line: l-value did not match symbol regex: %s",
+                l_value
+            );
+            exit(1);
+        }
+    } else {
+        // try and parse as a C-command
+        char *dest, *comp, *jump, *rest;
+        char *EQUALS_TOKEN = "=";
+        char *SEMICOLON_TOKEN = ";";
+
+        int equals_count = char_count(line_no_whitespace, '=');
+        switch (equals_count) {
+            case 0:
+                dest = NULL;
+                rest = line_no_whitespace;
+                break;
+            case 1:
+                char *saveptr_equals;
+                dest = strtok_r(line_no_whitespace, EQUALS_TOKEN, &saveptr_equals);
+                rest = strtok_r(NULL, EQUALS_TOKEN, &saveptr_equals);
+                break;
+            default:
+                fprintf(stderr, "parse_line: cannot parse line with more than one equals sign:\n%s", line);
+                exit(EXIT_FAILURE);
+        }
+
+        int semicol_count = char_count(rest, ';');
+        switch (semicol_count) {
+            case 0:
+                jump = NULL;
+                comp = rest;
+                break;
+            case 1:
+                char *saveptr_semicolon;
+                rest = strtok_r(rest, SEMICOLON_TOKEN, &saveptr_semicolon);
+                jump = strtok_r(NULL, SEMICOLON_TOKEN, &saveptr_semicolon);
+                break;
+            default:
+                fprintf(stderr, "parse_line: cannot parse line with more than one equals sign:\n%s", line);
+                exit(EXIT_FAILURE);
+        }
+
+        // FIXME: post-condition testing that dest+comp+jump are valid
+        struct Command command = {C_COMMAND, .dest=dest, .comp=comp, .jump=jump};
+        return command;
     }
 }
 
