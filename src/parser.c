9bef3bb240f562d7857982ed1226ccc6cba78b03
David Nicholson <nickledave@users.noreply.github.com>
2026-09-14 09:45:06 -0400

    Remove  from src/parser.c

--- src/parser.c
+++ src/parser.c
@@ -8,8 +8,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include <sys/types.h>
-
 #include "code.h"
 #include "parser.h"
 
@@ -257,17 +255,4 @@ struct Command parse_line(char line[]) {
         struct Command command = {C_COMMAND, .dest=dest, .comp=comp, .jump=jump};
         return command;
     }
-}
-
-
-void first_pass(FILE *fp) {
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t read;
-
-    while ((read = getline(&line, &len, fp)) != -1)
-    {
-        struct Command command = parse_line(line);
-    }
-
 }
