eb617cb5f1a3a8776cee7ea81e4435ae6cbec5dc
David Nicholson <nickledave@users.noreply.github.com>
2026-09-14 09:46:15 -0400

    WIP: Implement `assemble` (rewritten `first_pass`)

---
+++ src/assemble.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/types.h>
+
+#include "parser.h"
+#include "symbol_table.h"
+
+
+void assemble(FILE *fp) {
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+    int rom_address = 0;
+    struct SymbolAddressPair* symbol_table = new_symbol_table();
+
+    struct Command *parsed_commands = NULL;
+    int num_ca_commands_parsed = 0;
+
+    while ((read = getline(&line, &len, fp)) != -1)
+    {
+        struct Command command = parse_line(line);
+        switch (command.command_type)
+        {
+        case L_COMMAND:
+            /* code */
+            if (!is_symbol_in_table(command.symbol, symbol_table))
+                add_symbol_to_table(command.symbol, symbol_table);
+            break;
+        case C_COMMAND:
+        case A_COMMAND:
+            // do the re-alloc dance
+            num_ca_commands_parsed += 1;
+            struct Command *new_parsed_commands_ptr = (struct Command*) realloc(
+                    parsed_commands, num_ca_commands_parsed * sizeof(struct Command)
+                );
+            if (new_parsed_commands_ptr == NULL) {
+                fprintf(stderr, "parser::first_pass: reallocation of `parsed_commands` failed");
+                exit(EXIT_FAILURE);
+            }
+            parsed_commands = new_parsed_commands_ptr;
+            parsed_commands[num_ca_commands_parsed - 1] = command;
+            // and finally, increment `rom_address`
+            rom_address += 1;
+            break;
+        }
+    }
+}
