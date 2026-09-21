22683e97e3e41f3bfaa31db08834f3dbac95abcf
David Nicholson <nickledave@users.noreply.github.com>
2026-09-20 23:57:46 -0400

    Rewrite symbol table
    Rewrite based on Ben Klemens `dictionary` in
    *21st Century C*.
    Basically, an object-oriented like approach in C.

--- src/symbol_table.c
+++ src/symbol_table.c
@@ -6,37 +6,47 @@
 #include "symbol_table.h"
 
 
-struct SymbolAddressPair* new_symbol_table() {
-    struct SymbolAddressPair* ptr = NULL;
+SymbolTable * new_symbol_table(void) {
+    SymbolTable * out = malloc(sizeof(SymbolTable));
+    *out = (SymbolTable) { };
+    return out;
 }
 
 
-bool is_symbol_in_table(char * symbol, struct SymbolAddressPair* table) {
-    if (table == NULL) {
-        return false;
-    }
-    size_t table_len = (sizeof(table)) / sizeof(struct SymbolAddressPair);
-    for (int i = 0; i<table_len; i++) {
-        if (!strcmp(table[i].symbol, symbol)) {
-            return true;
+void free_symbol_table(SymbolTable * table) {
+    free(table);
+}
+
+
+int table_lookup_symbol(char * symbol, SymbolTable* table) {
+    for (int i = 0; i < table->len; i++) {
+        if (!strcmp(table->pairs[i].symbol, symbol)) {
+            return table->pairs[i].rom_address;
         }
     }
-    return false;
+    // -1 is sentinel value that means "symbol not found".
+    // this works well enough, since -1 is not a valid ROM address
+    return -1;
 }
 
 
-struct SymbolAddressPair* add_symbol_to_table(char *symbol, int rom_address, struct SymbolAddressPair* table) {
-    size_t table_len = (sizeof(table)) / sizeof(struct SymbolAddressPair);
-    size_t new_len = table_len + 1;
-    struct SymbolAddressPair* new_table_ptr = realloc(
-        table, sizeof(struct SymbolAddressPair) * new_len
+int add_symbol_to_table(char *symbol, int rom_address, struct SymbolTable* table) {
+    // pre-condition: symbol can't already be in table, return fail if it is
+    if (!(table_lookup_symbol(symbol, table) == -1)) {
+        return -1;
+    }
+
+    // ok, now add to table
+    table->len++;
+    struct SymbolAddressPair* new_pairs_ptr = realloc(
+        table->pairs, sizeof(struct SymbolAddressPair) * table->len
     );
-    if (new_table_ptr == NULL) {
-        fprintf(stderr, "symbol_table::add_symbol_to_table: realloc of table failed");
+    if (new_pairs_ptr == NULL) {
+        fprintf(stderr, "symbol_table::add_symbol_to_table: realloc of table->pairs failed");
         exit(EXIT_FAILURE);
     }
-    table = new_table_ptr;
+    table->pairs = new_pairs_ptr;
     struct SymbolAddressPair new_pair = {.symbol=symbol, .rom_address=rom_address};
-    table[new_len - 1] = new_pair;
-    return table;
+    table->pairs[table->len - 1] = new_pair;
+    return 0;
 }
--- src/symbol_table.h
+++ src/symbol_table.h
@@ -1,13 +1,22 @@
 #ifndef SYMBOL_TABLE_H
 #define SYMBOL_TABLE_H
 
-struct SymbolAddressPair {
+
+typedef struct SymbolAddressPair {
     char * symbol;
     int rom_address;
-};
+} SymbolAddressPair;
 
-struct SymbolAddressPair* new_symbol_table();
-bool is_symbol_in_table(char * symbol, struct SymbolAddressPair* table);
-struct SymbolAddressPair* add_symbol_to_table(char *symbol, int rom_address, struct SymbolAddressPair* table);
+
+typedef struct SymbolTable {
+    SymbolAddressPair *pairs;
+    int len;
+} SymbolTable;
+
+
+SymbolTable *new_symbol_table(void);
+void free_symbol_table(SymbolTable * table);
+int table_lookup_symbol(char * symbol, SymbolTable* table);
+int add_symbol_to_table(char *symbol, int rom_address, SymbolTable* table);
 
 #endif
